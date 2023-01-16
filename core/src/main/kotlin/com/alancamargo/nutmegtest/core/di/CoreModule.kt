8a9e937aa62dbc89a04627b6750dfdc5d731a9ff
Alan Camargo <alan.uk.br@gmail.com>
2023-01-16 19:04:23 +0000

    Implement LocalDatabaseProvider

--- core/src/main/kotlin/com/alancamargo/nutmegtest/core/di/CoreModule.kt
+++ core/src/main/kotlin/com/alancamargo/nutmegtest/core/di/CoreModule.kt
@@ -1,10 +1,14 @@
 package com.alancamargo.nutmegtest.core.di
 
+import android.content.Context
+import com.alancamargo.nutmegtest.core.database.LocalDatabaseProvider
+import com.alancamargo.nutmegtest.core.database.LocalDatabaseProviderImpl
 import com.alancamargo.nutmegtest.core.network.ApiProvider
 import com.alancamargo.nutmegtest.core.network.ApiProviderImpl
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
@@ -21,4 +25,10 @@ internal object CoreModule {
     @Provides
     @Singleton
     fun provideApiProvider(impl: ApiProviderImpl): ApiProvider = impl
+
+    @Provides
+    @Singleton
+    fun provideLocalDatabaseProvider(
+        @ApplicationContext context: Context
+    ): LocalDatabaseProvider = LocalDatabaseProviderImpl(context)
 }
