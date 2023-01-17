13753c88d0dfcd90b538849890d33269ee7276c3
Alan Camargo <alan.uk.br@gmail.com>
2023-01-17 18:41:23 +0000

    Fix database insertion

--- feature-albums/src/main/kotlin/com/alancamargo/nutmegtest/albums/data/database/AlbumDao.kt
+++ feature-albums/src/main/kotlin/com/alancamargo/nutmegtest/albums/data/database/AlbumDao.kt
@@ -2,6 +2,7 @@ package com.alancamargo.nutmegtest.albums.data.database
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.alancamargo.nutmegtest.albums.data.model.database.DbAlbum
 
@@ -11,6 +12,6 @@ internal interface AlbumDao {
     @Query("SELECT * FROM ALBUMS")
     suspend fun getAlbums(): List<DbAlbum>?
 
-    @Insert(entity = DbAlbum::class)
+    @Insert(entity = DbAlbum::class, onConflict = OnConflictStrategy.IGNORE)
     suspend fun insertAlbum(album: DbAlbum)
 }
