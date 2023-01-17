53958fdb69d3959c6b4200a2f875179b979a0032
Alan Camargo <alan.uk.br@gmail.com>
2023-01-17 22:37:58 +0000

    Writing UI tests

--- feature-albums/src/androidTest/kotlin/com/alancamargo/nutmegtest/albums/ui/AlbumListActivityTest.kt
+++ feature-albums/src/androidTest/kotlin/com/alancamargo/nutmegtest/albums/ui/AlbumListActivityTest.kt
@@ -2,11 +2,15 @@ package com.alancamargo.nutmegtest.albums.ui
 
 import com.alancamargo.nutmegtest.albums.ui.robot.onActivityLaunched
 import com.alancamargo.nutmegtest.core.design.dialogue.DialogueHelper
+import com.alancamargo.nutmegtest.core.test.getJsonFromAssets
+import com.alancamargo.nutmegtest.core.test.mockWebServer
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
+import okhttp3.mockwebserver.MockResponse
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
+import java.net.HttpURLConnection
 import javax.inject.Inject
 
 @HiltAndroidTest
@@ -25,6 +29,19 @@ class AlbumListActivityTest {
 
     @Test
     fun a() {
+        mockWebServer.enqueue(
+            MockResponse().setResponseCode(HttpURLConnection.HTTP_OK)
+                .setBody(getJsonFromAssets("albums_success.json"))
+        )
+        mockWebServer.enqueue(
+            MockResponse().setResponseCode(HttpURLConnection.HTTP_OK)
+                .setBody(getJsonFromAssets("users_success.json"))
+        )
+        mockWebServer.enqueue(
+            MockResponse().setResponseCode(HttpURLConnection.HTTP_OK)
+                .setBody(getJsonFromAssets("photos_success.json"))
+        )
+
         onActivityLaunched {
 
         }
--- feature-albums/src/androidTest/kotlin/com/alancamargo/nutmegtest/albums/ui/robot/AlbumListActivityRobot.kt
+++ feature-albums/src/androidTest/kotlin/com/alancamargo/nutmegtest/albums/ui/robot/AlbumListActivityRobot.kt
@@ -2,6 +2,7 @@ package com.alancamargo.nutmegtest.albums.ui.robot
 
 import androidx.test.core.app.ActivityScenario
 import com.alancamargo.nutmegtest.albums.ui.AlbumListActivity
+import com.alancamargo.nutmegtest.core.test.mockWebResponse
 
 fun onActivityLaunched(block: AlbumListActivityRobot.() -> Unit): AlbumListActivityRobot {
     ActivityScenario.launch(AlbumListActivity::class.java)
@@ -9,6 +10,18 @@ fun onActivityLaunched(block: AlbumListActivityRobot.() -> Unit): AlbumListActiv
 }
 
 class AlbumListActivityRobot {
+
+    fun mockSuccessfulAlbumsResponse() {
+        mockWebResponse(jsonAssetPath = "albums_success.json")
+    }
+
+    fun mockSuccessfulUsersResponse() {
+        mockWebResponse(jsonAssetPath = "users_success.json")
+    }
+
+    fun mockSuccessfulPhotosResponse() {
+        mockWebResponse(jsonAssetPath = "photos_success.json")
+    }
 
     infix fun perform(block: AlbumListActionRobot.() -> Unit): AlbumListActionRobot {
         return AlbumListActionRobot().apply(block)
